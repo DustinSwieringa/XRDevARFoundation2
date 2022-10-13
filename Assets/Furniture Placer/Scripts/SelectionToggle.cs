b88ad245e6ba53e281dc30af9d76820600bbbd82
Dustin Swieringa <dustin@circuitstream.com>
2022-10-13 14:33:01 -0600

    furniture Interactables

---
+++ Assets/Furniture Placer/Scripts/SelectionToggle.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SelectionToggle : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer _rendererToChange;
+
+    [SerializeField]
+    private Material _selectionMaterial;
+
+    private Material _defaultMaterial;
+
+    private void Awake()
+    {
+        _defaultMaterial = _rendererToChange.sharedMaterial;
+    }
+
+    private void OnEnable()
+    {
+        _rendererToChange.sharedMaterial = _selectionMaterial;
+    }
+
+    private void OnDisable()
+    {
+        _rendererToChange.sharedMaterial = _defaultMaterial;
+    }
+}
