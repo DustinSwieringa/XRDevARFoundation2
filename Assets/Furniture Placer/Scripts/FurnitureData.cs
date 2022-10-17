a5450a45035815f5cd554eb0cbf2ab7952d485d5
Dustin Swieringa <dustin@circuitstream.com>
2022-10-17 16:10:18 -0600

    Scriptable Objects

---
+++ Assets/Furniture Placer/Scripts/FurnitureData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FurnitureData : ScriptableObject
+{
+    public string furnitureName;
+    public Texture2D furnitureIcon;
+    public GameObject furniturePrefab;
+}
