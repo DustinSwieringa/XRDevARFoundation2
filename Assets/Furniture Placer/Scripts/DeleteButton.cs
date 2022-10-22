46aafc076920c337ca41a76ca5e3bdac137ad699
Dustin Swieringa <dustin@circuitstream.com>
2022-10-22 06:47:43 -0600

    Delete button

---
+++ Assets/Furniture Placer/Scripts/DeleteButton.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DeleteButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button _deleteButton;
+
+    private GameObject _currentSelectedGameObject;
+
+    private void Start()
+    {
+        _deleteButton.onClick.AddListener(ButtonClicked);
+    }
+
+    public void SelectEnter(SelectEnterEventArgs args)
+    {
+        _currentSelectedGameObject = args.interactableObject.transform.gameObject;
+        _deleteButton.interactable = true;
+    }
+
+    public void SelectExit(SelectExitEventArgs args)
+    {
+        _deleteButton.interactable = false;
+    }
+
+    private void ButtonClicked()
+    {
+        Destroy(_currentSelectedGameObject);
+    }
+}
